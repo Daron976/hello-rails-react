0aed2be40388700d2b7d459c7b79cfbfe55553dc
Daron976 <daronkwiafe@gmail.com>
2023-02-21 16:11:26 +0200

    Setup messages scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(msg: 'Good Morning')
+Message.create(msg: 'Hello there')
+Message.create(msg: 'Good day')
+Message.create(msg: 'Hey there')
+Message.create(msg: 'Good Evening')
